c79cbab6298ae567ccd360f187e4dfec36de8b5a
Masha Bannikova <masher@Denis-MacBook.local>
2023-01-11 13:50:44 +0200

    Initial commit

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,14 +1,8 @@
-import formHandler from "./modules/form-handler";
 import headerMenuToggle from "./modules/header-menu-toggle";
 import scrollMotion from "./modules/scroll-motion";
 
 // Init header menu toggle
 headerMenuToggle();
-
-formHandler('form-contact');
-
-// Init mouse depth
-// window.addEventListener('mousemove', mouseDepth);
 
 // Init scroll motion
 window.addEventListener('scroll', scrollMotion);
